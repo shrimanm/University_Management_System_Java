3dcb468293ef103904715aae38715a0b8b1aef5f
SHRIMAN N M <81467966+shrimanm@users.noreply.github.com>
2023-08-06 16:33:57 +0530

    Update About.java

--- src/university/management/system/About.java
+++ src/university/management/system/About.java
@@ -22,17 +22,12 @@ public class About extends JFrame {
         heading.setFont(new Font("Tahoma", Font.BOLD, 30));
         add(heading);
         
-        JLabel name = new JLabel("Developed By: Code for Interview");
+        JLabel name = new JLabel("Developed By: SHRIMAN");
         name.setBounds(70, 220, 550, 40);
         name.setFont(new Font("Tahoma", Font.BOLD, 30));
         add(name);
         
-        JLabel rollno = new JLabel("Roll number: 1533146");
-        rollno.setBounds(70, 280, 550, 40);
-        rollno.setFont(new Font("Tahoma", Font.PLAIN, 30));
-        add(rollno);
-        
-        JLabel contact = new JLabel("Contact: codeforinterview03@gmail.com");
+        JLabel contact = new JLabel("Contact: 18shrimanmogaveer@gmail.com");
         contact.setBounds(70, 340, 550, 40);
         contact.setFont(new Font("Tahoma", Font.PLAIN, 20));
         add(contact);
